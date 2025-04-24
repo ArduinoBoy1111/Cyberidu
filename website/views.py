86bce7f83e1695b6eab0e6e434856f3433b0984f
SAFAA1981 <safaasaadoon@gmail.com>
2025-04-25 00:03:23 +0300

    curosle_addition

--- website/views.py
+++ website/views.py
@@ -5,7 +5,57 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("base.html")
+    projects = [
+    {
+        "name": "حساس الطقس",
+        "image": "arduino_project1.png",
+        "price": 49.99,
+        "description": "حساس لقياس درجة الحرارة والرطوبة",
+        "new": True
+    },
+    {
+        "name": "المنزل الذكي",
+        "image": "arduino_project2.jpg",
+        "price": 89.99,
+        "description": "نظام أمان منزلي ذكي مع كاميرا مراقبة"
+    },
+    {
+        "name": "ذراع روبوتية",
+        "image": "arduino_project3.jpg",
+        "price": 129.99,
+        "description": "ذراع روبوتية قابلة للبرمجة للتحكم عن بعد",
+        "new": True
+    },
+    {
+        "name": "مشروع تجريبي 1",
+        "image": "test_image.png",
+        "price": 15.00,
+        "description": "وصف افتراضي لمشروع تجريبي لغرض الاختبار",
+        "new": False
+    },
+    {
+        "name": "مشروع جديد 2",
+        "image": "test_image.png",
+        "price": 25.50,
+        "description": "مشروع تجريبي مع علامة جديد",
+        "new": True
+    },
+    {
+        "name": "نظام مراقبة",
+        "image": "test_image.png",
+        "price": 60.00,
+        "description": "نظام تجريبي لمراقبة البيئة الداخلية",
+        "new": False
+    },
+    {
+        "name": "تطبيق مدرسي",
+        "image": "test_image.png",
+        "price": 40.00,
+        "description": "تطبيق بسيط لتجربة عرض المشاريع",
+        "new": True
+    }
+    ]
+    return render_template("base.html",projects=projects)
 
 @views.route('/about')
 def about():
